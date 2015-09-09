a52a19b6d8e440ff59a99a42d42ad858dc07d4d2
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2015-09-09 22:47:21 +0200

    Add Elementary GTK theme.

---
+++ gtk/themes/elementary.rb
@@ -0,0 +1,15 @@
+version = 4.0.6
+
+class GtkElementaryTheme < Formula
+  desc "Elementary GTK+ 2.0/3.0 theme"
+  homepage "https://launchpad.net/egtk"
+  url "https://launchpad.net/egtk/4.x/#{version}/+download/elementary.tar.xz"
+  md5 "d2a39e5842dd157edbed00992090730c"
+
+  depends_on "gtk-murrine-engine"
+  depends_on "gtk+"
+
+  def install
+    (share/'themes').install 'elementary'
+  end
+end
