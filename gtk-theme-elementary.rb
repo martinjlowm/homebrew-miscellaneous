9e5299f08ea545af075c18fcfddd4e86b36fc932
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2015-09-09 23:13:52 +0200

    Single quotes and use sha256.

--- gtk-theme-elementary.rb
+++ gtk-theme-elementary.rb
@@ -1,12 +1,12 @@
 class GtkThemeElementary < Formula
   version '4.0.6'
-  desc "Elementary GTK+ 2.0/3.0 theme"
-  homepage "https://launchpad.net/egtk"
+  desc 'Elementary GTK+ 2.0/3.0 theme'
+  homepage 'https://launchpad.net/egtk'
   url "https://launchpad.net/egtk/4.x/#{version}/+download/elementary.tar.xz"
-  md5 "d2a39e5842dd157edbed00992090730c"
+  sha256 'eed59e6957b9baf050fb762ec9455a98d35471f0034dddb91fedcdafcefa5963'
 
-  depends_on "gtk-murrine-engine"
-  depends_on "gtk+"
+  depends_on 'gtk-murrine-engine'
+  depends_on 'gtk+'
 
   def install
     (share/'themes').install 'elementary'
