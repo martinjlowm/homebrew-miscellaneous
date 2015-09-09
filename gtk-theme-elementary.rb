e86c0058a06039a89d4e5b482a27ec628e1460f6
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2015-09-09 23:07:59 +0200

    Move version.

--- gtk-theme-elementary.rb
+++ gtk-theme-elementary.rb
@@ -1,6 +1,5 @@
-version = '4.0.6'
-
 class GtkThemeElementary < Formula
+  version '4.0.6'
   desc "Elementary GTK+ 2.0/3.0 theme"
   homepage "https://launchpad.net/egtk"
   url "https://launchpad.net/egtk/4.x/#{version}/+download/elementary.tar.xz"
